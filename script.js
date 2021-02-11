18e7e947a70e78c238316b19821ef50d41f23d04
suma579 <51452904+suma579@users.noreply.github.com>
2021-02-11 13:57:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("To get in please enter the key!!!");
+  	if(a== "pass")
+  	{
+  	    window.location.href="home.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
